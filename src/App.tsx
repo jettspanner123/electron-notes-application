76483d5c0efd00c039a0a4c10446a03a18602719
Uddeshya Singh <106103684+jettspanner123@users.noreply.github.com>
2025-06-17 00:52:51 +0530

    project init

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './App.css'
+
+function App() {
+
+  return (
+      <React.Fragment>
+          <main className={`bg-[#212121] text-white h-screen w-screen flex`}>
+
+
+              {/*this is the login section*/}
+              <section className={`flex-1 bg-blue-300 h-full`}>
+
+              </section>
+
+
+
+              {/*this is the photo section*/}
+              <section className={`flex-1/5 bg-green-300 h-full`}>
+
+              </section>
+          </main>
+      </React.Fragment>
+  )
+}
+
+export default App
